6cb38472eecceaf7ed46ad593b82a0c3fdbf494b
TwaMarta <82045392+TwaMarta@users.noreply.github.com>
2021-12-14 14:22:49 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = (selector) => document.querySelector(selector);
+const galleryAll = (selector) => document.querySelectorAll(selector);
+
+
+const galleryEle = gallery(".gallery");
+console.log(galleryEle);
+
+const images = galleryItems
+    .map(item =>
+        `<a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`
+    
+    )
+    .join("");
+
+galleryEle.insertAdjacentHTML("afterbegin", images);
+console.log(images);
+
+let lightbox = new SimpleLightbox(`.gallery a`, {
+    captionsData: "alt",
+    captionsDelay: 250
+});
